b3b2920f7dbac05b73a683c8ee6ba5bf9c807d41
Nadiia-Y <115303631+NadiiaYena@users.noreply.github.com>
2024-05-07 15:55:45 +0300

    Delete src directory

--- src/scripts/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('.header-menu');
-    if (window.scrollY > 0) {
-        header.classList.add('scroll');
-    } else {
-        header.classList.remove('scroll');
-    }
-});
-
